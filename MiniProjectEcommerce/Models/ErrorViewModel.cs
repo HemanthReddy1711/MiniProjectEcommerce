e36683a7cef6ff69cac1574499dcdf8dcaf0b76f
HemanthReddy1711 <reddy.reddy@globallogic.com>
2022-07-27 11:22:26 +0530

    Revert "Full MiniProjectEcommerce"
    This reverts commit 3b0a9c29212ee4b102a77238118114597deb3e86.

--- MiniProjectEcommerce/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MiniProjectEcommerce.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
